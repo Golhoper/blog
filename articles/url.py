bcf238bfd259d100ea5a6b7821b4060979c71cd0
Golhoper <dead3@bk.ru>
2018-08-27 10:23:32 +0300

    fourth part

--- articles/url.py
+++ articles/url.py
@@ -8,6 +8,4 @@ urlpatterns = [
     path('', main_us, name="main"),
     re_path('^delete/(?P<id>\w+)/$', delete_art, name="delete"),
     re_path('^change/(?P<id>\w+)/$', change_art, name="change"),
-    # path('profile/',TemplateView.as_view(template_name='profile.html')),
-    # path('saved/', SaveProfile, name='saved'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
